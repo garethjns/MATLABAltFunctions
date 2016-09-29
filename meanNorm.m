0dadc1b965a1e1e004c52e92cfbfd00e4a24c1fd
garethjns <garethjns4@gmail.com>
2016-09-29 12:04:11 +0100

    Updated meanNorm
    Now uses bsxfun instead of loop and deals with NaNs

--- meanNorm.m
+++ meanNorm.m
@@ -1,11 +1,24 @@
 function [xNorm, dataMean, dataSig] = meanNorm(x)
+% Replaces zscore, deals with nans
 % xNorm = (x-mean(x)) / std(x);
 
-dataMean = mean(x);
-dataSig = std(x);
-
-xNorm = NaN(size(x));
-for c = 1:size(x,2)
-    xNorm(:,c) = (x(:,c)-dataMean(1,c)) ./ dataSig(1,c); 
+% Check if nanmean is available and needed.
+% nanamean is slower than mean and it's ~25% faster to check for NaNs 
+% than to just just use nanmean if it's available
+if exist('nanmean.m','file') && any(any(isnan(x)))
+    meanFcn = @nanmean;
+    stdFcn = @nanstd;
+else
+    meanFcn = @mean;
+    stdFcn = @std;
 end
 
+% Get stats
+dataMean = meanFcn(x);
+dataSig = stdFcn(x);
+% Avoid dividing by 0
+dataSig(dataSig==0) = 1;
+
+% Two bsxfun calls are quicker than for loop
+xNorm = bsxfun(@minus, x, dataMean);
+xNorm = bsxfun(@rdivide, xNorm, dataSig);
